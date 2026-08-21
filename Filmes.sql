1ad78d023fde12fc43d22fb99ab06c478cbddad3
Caua <cauasantiagofreitas23241@gmail.com>
2026-08-20 21:40:36 -0300

    First commit

---
+++ Filmes.sql
@@ -0,0 +1,20 @@
+insert into filme (titulo,genero,ano,visto)
+	values('Titanic', 'Drama', '1997', '0');
+    
+insert into filme (titulo,genero,ano,visto)
+	values('Up Altas Aventuras', 'Aventura', '2009', '0');
+    
+insert into filme (titulo,genero,ano,visto)
+	values('Wall-e', 'Aventura', '2008', '0');
+    
+insert into filme (titulo,genero,ano,visto)
+	values('Divertida mente', ' Aventura', '2015', '3');
+    
+insert into filme (titulo,genero,ano,visto)
+	values('Divertida mente','Aventura', '2025', '1');
+    
+insert into filme (titulo,genero,ano,visto)
+	values('Bolt-Surpercão','Aventura', '2008', '0');
+    
+insert into filme (titulo,genero,ano,visto)
+	values('Ratatouille','Aventura', '2007', '2');
